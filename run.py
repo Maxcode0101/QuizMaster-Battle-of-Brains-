00bcd8d59f34177cf2e6cd79e58d12fc3941c078
Max Kaening <maximilian.kaening@gmail.com>
2024-09-25 11:18:08 +0000

    Creating questionlist file and importing questions to main, defining for loop

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
-print("QuizMaster")
+# Quizmaster : Battle of brains
+
+# Imports list with all mathquestions from questionlist.py file
+from questionlist import mathquestions
+
+# Userscore in the beginning
+score = 0
+
+# Looping through the mathquestions
+for question in mathquestions:
+    question_text = question.get("question")
+    print(question_text)
+    print("Choices: ")
+    print(question.get("options"))
+
+    user_answer = input("Enter answer: ")
+    correct_answer = question.get("answer")
+
+    if user_answer == correct_answer:
+        score = score + 10
+        
+print("Your score = ", score)
