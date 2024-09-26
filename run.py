0aa4a46012a34145569beb1616a22f8462bbc456
Max Kaening <maximilian.kaening@gmail.com>
2024-09-26 12:18:42 +0000

    Importing modules

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 # Quizmaster : Battle of brains
 
+# Imports
+
+# Import colorama module
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+# Import pyfiglet module 
+import pyfiglet 
+
+# Import random to use random numbers for Quizmaster
+import random
+
+import time
+import os
+
 # Imports list with all mathquestions from questionlist.py file
 from questionlist import mathquestions
 
@@ -10,13 +26,22 @@ score = 0
 for question in mathquestions:
     question_text = question.get("question")
     print(question_text)
+    print("--------------")
     print("Choices: ")
     print(question.get("options"))
-
-    user_answer = input("Enter answer: ")
-    correct_answer = question.get("answer")
+    print("--------------")
+   
+    user_answer = int(input("Enter answer: ")) 
+    correct_answer = int(question.get("answer"))
 
     if user_answer == correct_answer:
+        print(f"{Fore.GREEN}Correct! ✅")
         score = score + 10
-        
-print("Your score = ", score)
+    else:
+        print(f"{Fore.RED}Wrong ❌, the correct answer is {correct_answer}")
+    
+
+# Calling functions
+start_game = welcome()
+
+print(f"Your score = {score}")
