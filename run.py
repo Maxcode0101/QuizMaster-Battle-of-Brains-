0c089af4441baa7dd0d288e44c35c40364cb9da3
Max Kaening <maximilian.kaening@gmail.com>
2024-09-26 18:24:00 +0000

    Wrapping loop into function, adding styling

--- run.py
+++ run.py
@@ -19,29 +19,39 @@ import os
 # Imports list with all mathquestions from questionlist.py file
 from questionlist import mathquestions
 
+
+# Global Variables
+
 # Userscore in the beginning
-score = 0
+SCORE = 0
+
+PLAYER = ""
+GAME = None
 
 # Looping through the mathquestions
-for question in mathquestions:
-    question_text = question.get("question")
-    print(question_text)
-    print("--------------")
-    print("Choices: ")
-    print(question.get("options"))
-    print("--------------")
-   
-    user_answer = int(input("Enter answer: ")) 
-    correct_answer = int(question.get("answer"))
-
-    if user_answer == correct_answer:
-        print(f"{Fore.GREEN}Correct! ✅")
-        score = score + 10
-    else:
-        print(f"{Fore.RED}Wrong ❌, the correct answer is {correct_answer}")
+def loop_questions():
+    """
+    Main function
+    """
+    global SCORE
+    for question in mathquestions:
+        question_text = question.get("question")
+        print(question_text)
+        print("--------------")
+        print("Choices: ")
+        print(question.get("options"))
+        print("--------------")
     
+        user_answer = int(input("Enter answer: ")) 
+        correct_answer = int(question.get("answer"))
+
+        if user_answer == correct_answer:
+            print(f"{Fore.GREEN}{correct_answer} is correct! ✅")
+            print(f"{Fore.GREEN}Your score = {SCORE+10}")
+        else:
+            print(f"{Fore.RED}Wrong ❌, the correct answer is {correct_answer}")
+            print(f"{Fore.RED}Your score = {SCORE}")
+        
 
 # Calling functions
-start_game = welcome()
-
-print(f"Your score = {score}")
+loop_questions()
